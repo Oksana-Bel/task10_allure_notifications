1f920a7abd3273adaa5caf175b02990ac16edd45
Oksana-Bel <belousova.oksana.work@gmail.com>
2021-04-18 10:27:31 -0400

    Added testteacher

--- src/test/java/TestEgarGlobal.java
+++ src/test/java/TestEgarGlobal.java
@@ -1,60 +1,60 @@
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestEgarGlobal {
-
-
-        @Test
-        void some1Test() {
-            assertTrue(true);
-        }
-
-        @Test
-        void some2Test() {
-            assertTrue(true);
-        }
-    }
-
-        /*void SpeakToUs () {
-@Test
-    void StartNow() {
-        Configuration.timeout=30000;
-        open("https://www.egarglobal.com");
-        // $(byText("Learn More")).click();
-        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
-        $(".webBtn",3).click();
-        $("h1").shouldHave(text("Contact an Expert"));
+public class TestEgarGlobal  {
+    @Test
+    void some1Test() {
+        assertTrue(true);
     }
 
     @Test
-
-        open("https://www.egarglobal.com");
-        // $(byText("Learn More")).click();
-        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
-        $(".webBtn",2).click();
-        $("h1").shouldHave(text("Contact an Expert"));
+    void some2Test() {
+        assertTrue(true);
     }
 
-    /*@Test
-    void LearnMoreAboutEGARGlobal () {
-        open("https://www.egarglobal.com");
-        // $(byText("Learn More")).click();
-        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
-        $(".webBtn",1).click();
-        $("h1").shouldHave(text("Enterprise Financial Services Software Meets"));
-    }
     @Test
-    void LearnMore() {
-        open("https://www.egarglobal.com");
-        // $(byText("Learn More")).click();
-        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
-        $(".webBtn",0).click();
-        $("h1").shouldHave(text(" Build Apps. Modify Systems."));
+    void some3Test() {
+        assertTrue(true);
     }
-    */
 
+    @Test
+    void some4Test() {
+        assertTrue(true);
+    }
 
+    @Test
+    void some5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some9Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some10Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    void some11Test() {
+        assertTrue(true);
+    }
+}
