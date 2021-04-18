cb436b6a912b18d8253289862eb6054552f10301
Oksana-Bel <belousova.oksana.work@gmail.com>
2021-04-18 06:40:36 -0400

    Added egar tests4

--- src/test/java/TestEgarGlobal.java
+++ src/test/java/TestEgarGlobal.java
@@ -1,5 +1,3 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,19 +5,40 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TestEgarGlobal {
 
-    @BeforeAll
-    static void setup() {
-        Configuration.startMaximized = true;
-    }
-
     @Test
-    void homePage() {
-        Configuration.timeout=6000;
+    void StartNow() {
+        //Configuration.timeout=6000;
         open("https://www.egarglobal.com");
         // $(byText("Learn More")).click();
         $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
         $(".webBtn",3).click();
         $("h1").shouldHave(text("Contact an Expert"));
+    }
+
+    @Test
+    void SpeakToUs () {
+        open("https://www.egarglobal.com");
+        // $(byText("Learn More")).click();
+        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
+        $(".webBtn",2).click();
+        $("h1").shouldHave(text("Contact an Expert"));
+    }
+
+    @Test
+    void LearnMoreAboutEGARGlobal () {
+        open("https://www.egarglobal.com");
+        // $(byText("Learn More")).click();
+        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
+        $(".webBtn",1).click();
+        $("h1").shouldHave(text("Enterprise Financial Services Software Meets"));
+    }
+    @Test
+    void LearnMore() {
+        open("https://www.egarglobal.com");
+        // $(byText("Learn More")).click();
+        $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
+        $(".webBtn",0).click();
+        $("h1").shouldHave(text(" Build Apps. Modify Systems."));
     }
 
 }
