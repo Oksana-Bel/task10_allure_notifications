4d7888e5a53338d98d6a7a19aed475f93e3c1fa2
Oksana-Bel <belousova.oksana.work@gmail.com>
2021-04-18 08:29:10 -0400

    Added build.gradle

--- src/test/java/TestEgarGlobal.java
+++ src/test/java/TestEgarGlobal.java
@@ -3,10 +3,24 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestEgarGlobal {
 
-    @Test
+    public class PositiveTests {
+        @Test
+        void some1Test() {
+            assertTrue(true);
+        }
+
+        @Test
+        void some2Test() {
+            assertTrue(true);
+        }
+    }
+
+        /*void SpeakToUs () {
+@Test
     void StartNow() {
         Configuration.timeout=30000;
         open("https://www.egarglobal.com");
@@ -17,7 +31,7 @@ public class TestEgarGlobal {
     }
 
     @Test
-    void SpeakToUs () {
+
         open("https://www.egarglobal.com");
         // $(byText("Learn More")).click();
         $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
@@ -32,7 +46,7 @@ public class TestEgarGlobal {
         $("h1").shouldHave(text("The NoCode platform for next-generation applications"));
         $(".webBtn",1).click();
         $("h1").shouldHave(text("Enterprise Financial Services Software Meets"));
-    }*/
+    }
     @Test
     void LearnMore() {
         open("https://www.egarglobal.com");
@@ -41,5 +55,6 @@ public class TestEgarGlobal {
         $(".webBtn",0).click();
         $("h1").shouldHave(text(" Build Apps. Modify Systems."));
     }
+    */
 
 }
